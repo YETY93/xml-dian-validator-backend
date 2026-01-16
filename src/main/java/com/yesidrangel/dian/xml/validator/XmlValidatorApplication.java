65f261d9c4f2a29ba4dc3e0b5d18a9d6bd5964b5
Yesid Rangel <yety93@hotmail.com>
2026-01-15 22:15:35 -0500

    Add complete XML validator project

---
+++ src/main/java/com/yesidrangel/dian/xml/validator/XmlValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.yesidrangel.dian.xml.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlValidatorApplication.class, args);
+	}
+
+}
